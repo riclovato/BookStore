8414b234868c0789c1d09aa7761ca52cce655763
riclovato <riic.lovato@gmail.com>
2023-05-28 14:52:24 -0300

    first commit

---
+++ server/src/test/java/com/rick/bookStore/BookStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rick.bookStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
